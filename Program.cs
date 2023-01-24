72c448ec03172141d625e2132a1c4894409284f5
ErenYilmaz41 <121278695+ErenYilmaz41@users.noreply.github.com>
2023-01-24 09:43:53 +0300

    First_update_degiskenler

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace degisken
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int deger;
+            deger = 2;
+            Console.WriteLine(deger);
+
+            byte b=5;
+            sbyte c=5;
+            short s=5;
+            ushort us=5;
+            Int16 i16 =2;
+            int i=2;
+            Int32 i32=2;
+            float f=5;
+            double d=5;
+            decimal de=5;
+            char ch='2';
+            bool b1=true;
+            bool b2=false;
+            DateTime dt=DateTime.Now;
+            Console.WriteLine(dt);
+        }
+    }
+}
